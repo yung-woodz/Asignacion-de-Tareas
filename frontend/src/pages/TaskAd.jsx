d88ce6a36b441d5a8332d25ed4b1c7b63e100bdd
Fran <francisca.rabanal2201@alumnos.ubiobio.cl>
2024-07-29 21:10:38 -0400

    service y el react de creacion

---
+++ frontend/src/pages/TaskAd.jsx
@@ -0,0 +1,11 @@
+import Form from "../components/Form";
+import Navbar from "../components/Navbar";
+import { createTask } from '../services/Task.service';
+
+const TaskAd = () => {
+ 
+    
+
+}
+
+export default TaskAd;
---
+++ frontend/src/services/Task.service.js
@@ -0,0 +1,37 @@
+import axios from './root.service.js';
+// crear las tareas
+export async function createTask(data) {
+    try {
+      const response = await axios.post('/works/task', data);
+      return response.data;
+    } catch (error) {
+      throw new Error('Error en la creacion de Tarea');
+    }
+  }
+// mostrar las tareas
+  export async function getTasks() {
+    try {
+      const response = await instance.get('/works/tasks');
+      return response.data;
+    } catch (error) {
+      throw new Error('No se han encontrado las Tareas');
+    }
+  }
+  
+  export async function updateTask(data, _id) {
+    try {
+      const response = await axios.patch('/works/tasks/status?_id=${_id}', data);
+      return response.data;
+    } catch (error) {
+      throw new Error('Error al Actualizar tarea');
+    }
+  }
+
+  export async function deleteTask(data) {
+    try {
+        const response = await axios.delete('/works/tasks/status/eliminar', data);
+        return response.data;
+    } catch (error) {
+        throw error.response?.data || error.message;
+    }
+}
